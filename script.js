bcae1f47bb2573d5b4ed1616e0cadf5e7b3934e4
Many18a <cabrerahenry593@gmail.com>
2026-02-27 22:55:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function saludo() {
+    alert("Quiza no sea un detalle muy normal, pero espero que lo sientas muy especial");
+}
+
+function PCA() {
+    window.open("https://www.youtube.com/watch?v=q3yJ_yDhNVk&list=RDq3yJ_yDhNVk&start_radio=1", "_blank");
+}
+
+function PCB() {
+    window.open("https://www.youtube.com/watch?v=fgXDHQm5eq4&list=RDfgXDHQm5eq4&start_radio=1", "_blank");
+}
+
+function PCC() {
+    window.open("https://www.youtube.com/watch?v=23wz86ZbD-o&list=RD23wz86ZbD-o&start_radio=1", "_blank");
+}
